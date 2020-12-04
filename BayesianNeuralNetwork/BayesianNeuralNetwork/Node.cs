e1f38fc010265588f57272c88a83ad2749a2b1df
Zalinca Claudiu <claudiuzalinca@gmail.com>
2020-12-04 08:03:25 +0200

    First Commit

---
+++ BayesianNeuralNetwork/BayesianNeuralNetwork/Node.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BayesianNeuralNetwork
+{
+    public class Node
+    {
+        public List<Node> parents;
+        public List<Node> sons;
+        public List<List<float>> probabilityTable;
+        public float trueProbabilityNow;
+        public string tag;
+        public int states;
+
+        public Node(string tag)
+        {
+            this.tag = tag;
+            parents = new List<Node>();
+            sons = new List<Node>();
+            probabilityTable = new List<List<float>>();
+            states=2;
+        }
+
+        public void AddSon(Node node)
+        {
+            sons.Add(node);
+            node.parents.Add(this);
+        }
+
+        public float True()
+        {
+            if (parents.Count != 0)
+            {
+                return True(new List<Tuple<Node, int>>());
+            }
+            else
+            {
+                return probabilityTable[0][1];
+            }
+        }
+
+        public float True(List<Tuple<Node,int>> dependents)
+        {
+            if (dependents.Count < this.parents.Count)
+            {
+                int lastIndex = dependents.Count();
+                var falseDependent = new List<Tuple<Node, int>>();
+                falseDependent.AddRange(dependents);
+                var trueDependent = new List<Tuple<Node, int>>();
+                trueDependent.AddRange(dependents);
+                var falseNewParent = new Tuple<Node, int>(parents[lastIndex], 0);
+                var trueNewParent = new Tuple<Node, int>(parents[lastIndex], 1);
+                trueDependent.Add(trueNewParent);
+                falseDependent.Add(falseNewParent);
+                return True(trueDependent) + True(falseDependent);
+            }
+            else
+            {
+                float p = getTrueProbability(dependents);
+                foreach(Tuple<Node,int> tuple in dependents)
+                {
+                    float dP = tuple.Item2 == 0 ? tuple.Item1.False() : tuple.Item1.True();
+                    p = p * dP;
+                }
+                return p;
+            }
+        }
+
+        public float False()
+        {
+            return 1 - True();
+        }
+
+        public float getTrueProbability(List<Tuple<Node,int>> parentValues)
+        {
+            double index = 0;
+            for (int i = 0; i < parents.Count; ++i)
+            {
+                double indP = parents.IndexOf(parentValues[i].Item1);
+                index += Math.Pow(2, indP) * parentValues[i].Item2;
+            }
+            return probabilityTable[(int)index][1];
+        }
+
+        public float getFalseProbability(List<Tuple<Node, int>> parentValues)
+        {
+            return 1 - getTrueProbability(parentValues);
+        }
+
+    }
+}
---
+++ BayesianNeuralNetwork/BayesianNeuralNetwork/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BayesianNeuralNetwork
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            LoadData();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static List<Node> LoadData()
+        {
+            List<Node> graph = new List<Node>();
+            Node Asia = new Node("Asia");
+            Asia.probabilityTable.Add(new List<float> { 0.99f, 0.01f });
+            Node Tub = new Node("Has Tuberculosis");
+            Asia.AddSon(Tub);
+            Tub.probabilityTable.Add(new List<float> { 0.99f, 0.01f });
+            Tub.probabilityTable.Add(new List<float> { 0.95f, 0.05f });
+            Node Smoker = new Node("Smoker");
+            Smoker.probabilityTable.Add(new List<float> { 0.5f, 0.5f });
+            Node Lung = new Node("Has Lung Cancer");
+            Smoker.AddSon(Lung);
+            Lung.probabilityTable.Add(new List<float> { 0.99f, 0.01f });
+            Lung.probabilityTable.Add(new List<float> { 0.9f, 0.1f });
+            Node Bronchite = new Node("Has Bronchitis");
+            Smoker.AddSon(Bronchite);
+            Bronchite.probabilityTable.Add(new List<float> { 0.7f, 0.3f });
+            Bronchite.probabilityTable.Add(new List<float> { 0.4f, 0.6f });
+            Node Either = new Node("Tuberculosis or Cancer");
+
+            Tub.AddSon(Either);
+            Lung.AddSon(Either);
+            
+            Either.probabilityTable.Add(new List<float> { 1.0f, 0.0f });
+            Either.probabilityTable.Add(new List<float> { 0.0f, 1.0f });
+            Either.probabilityTable.Add(new List<float> { 0.0f, 1.0f });
+            Either.probabilityTable.Add(new List<float> { 0.0f, 1.0f });
+            Node Dyspnea = new Node("Dyspnea");
+            Either.AddSon(Dyspnea);
+            Bronchite.AddSon(Dyspnea);
+           
+            Dyspnea.probabilityTable.Add(new List<float> { 0.9f, 0.1f });
+            Dyspnea.probabilityTable.Add(new List<float> { 0.3f, 0.7f });
+            Dyspnea.probabilityTable.Add(new List<float> { 0.2f, 0.8f });
+            Dyspnea.probabilityTable.Add(new List<float> { 0.1f, 0.9f });
+            Node Xray = new Node("XRay Result");
+            Either.AddSon(Xray);
+            Xray.probabilityTable.Add(new List<float> { 0.95f, 0.05f });
+            Xray.probabilityTable.Add(new List<float> { 0.02f, 0.98f });
+            graph.Add(Asia);
+            graph.Add(Tub);
+            graph.Add(Smoker);
+            graph.Add(Bronchite);
+            graph.Add(Lung);
+            graph.Add(Either);
+            graph.Add(Dyspnea);
+            graph.Add(Xray);
+
+            /*for(int i = 0; i < graph.Count; ++i)
+            {
+                Console.WriteLine($"{graph[i].tag} -> {graph[i].True()*100}%");
+            }*/
+
+            
+
+            var dependents = new List<Tuple<Node, int>>();
+            dependents.Add(new Tuple<Node, int>(Lung,1));
+            
+
+            for (int i = 0; i < graph.Count; ++i)
+            {
+                Console.WriteLine($"{graph[i].tag} -> {queryProbability(graph[i],dependents,graph) * 100}%");
+            }
+
+            
+            dependents.Add(new Tuple<Node, int>(Bronchite, 0));
+
+            for (int i = 0; i < graph.Count; ++i)
+            {
+                Console.WriteLine($"{graph[i].tag} -> {queryProbability(graph[i], dependents, graph) * 100}%");
+            }
+
+            dependents.Add(new Tuple<Node, int>(Dyspnea, 0));
+
+            for (int i = 0; i < graph.Count; ++i)
+            {
+                Console.WriteLine($"{graph[i].tag} -> {queryProbability(graph[i], dependents, graph) * 100}%");
+            }
+
+            return graph;
+        }
+
+        public static float queryProbability(Node node, List<Tuple<Node, int>> dependents, List<Node> graph)
+        {
+            var depF = new List<Tuple<Node, int>>();
+            var depT = new List<Tuple<Node, int>>();
+            depF.AddRange(dependents);
+            depT.AddRange(dependents);
+            depT.Add(new Tuple<Node, int>(node, 1));
+            depF.Add(new Tuple<Node, int>(node, 0));
+            var t = recursive(depT, graph, 0);
+
+            var f = recursive(depF, graph, 0);
+            if (t + f < 1)
+            {
+                var alpha = 1.0f / (t + f);
+                return alpha * t;
+            }
+            else
+            {
+                return t;
+            }
+
+            
+        }
+
+
+        public static float recursive(List<Tuple<Node,int>> dependents, List<Node> graph, int ind)
+        {
+            if (ind<graph.Count) {
+                var depF = new List<Tuple<Node, int>>();
+                var depT = new List<Tuple<Node, int>>();
+                depF.AddRange(dependents);
+                depT.AddRange(dependents);
+                depF.Add(new Tuple<Node, int>(graph[ind], 0));
+                depT.Add(new Tuple<Node, int>(graph[ind], 1));
+                if (dependents.FirstOrDefault(d => d.Item1 == graph[ind]) != null)
+                {
+                    if (dependents.First(d => d.Item1 == graph[ind]).Item2 == 0)
+                    {
+                        return graph[ind].getFalseProbability(getParentsValues(dependents, graph[ind].parents)) * recursive(depF, graph, ind + 1);
+                        
+                    }
+                    else
+                    {
+                        return graph[ind].getTrueProbability(getParentsValues(dependents, graph[ind].parents)) * recursive(depT, graph, ind + 1);
+                    }
+                }
+                else
+                {
+                    return graph[ind].getFalseProbability(getParentsValues(dependents, graph[ind].parents)) * recursive(depF, graph, ind + 1) +
+                        graph[ind].getTrueProbability(getParentsValues(dependents, graph[ind].parents)) * recursive(depT, graph, ind + 1);
+                }
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public static bool checkParentsAreInDependents(List<Tuple<Node,int>> dependents, List<Node> parents)
+        {
+            foreach(Node node in parents)
+            {
+                if (dependents.Select(i => i.Item1).FirstOrDefault(n => n == node) == null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static List<Tuple<Node, int>> getParentsValues(List<Tuple<Node, int>> dependents, List<Node> parents)
+        {
+            var result = new List<Tuple<Node, int>>();
+            foreach (Node node in parents)
+            {
+                var x = dependents.FirstOrDefault(n => n.Item1 == node);
+                if (x != null)
+                {
+                    result.Add(x);
+                }
+            }
+            return result;
+        }
+
+        public static string afisare(List<Tuple<Node, int>> dependents, List<Node> graph, int ind)
+        {
+            if (ind < graph.Count)
+            {
+                var depF = new List<Tuple<Node, int>>();
+                var depT = new List<Tuple<Node, int>>();
+                depF.AddRange(dependents);
+                depT.AddRange(dependents);
+                depF.Add(new Tuple<Node, int>(graph[ind], 0));
+                depT.Add(new Tuple<Node, int>(graph[ind], 1));
+                if (dependents.FirstOrDefault(d => d.Item1 == graph[ind]) != null)
+                {
+                    if (dependents.First(d => d.Item1 == graph[ind]).Item2 == 0)
+                    {
+                        return $"PN({graph[ind].tag})*{afisare(depF, graph, ind + 1)}";
+
+                    }
+                    else
+                    {
+                        return $"PD({graph[ind].tag})*{afisare(depT, graph, ind + 1)}";
+                    }
+                }
+                else
+                {
+                    return $"Sum({graph[ind].tag})";
+                }
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+    }
+}
